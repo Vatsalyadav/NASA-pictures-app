3c92198e5323b9a43c6f2bb0851c3575b41fea9d
Vatsal Yadav <vatsalyad15@gmail.com>
2020-02-21 18:41:45 +0530

    Rename MainActivity file to PhotosGridActivity
    MainActivity file renamed and its references refactored
     in activity_photos_grid.xml and AndroidManifest.xml

--- app/src/main/java/com/example/nasapictures/view/PhotosGridActivity.java
+++ app/src/main/java/com/example/nasapictures/view/PhotosGridActivity.java
@@ -1,14 +1,16 @@
-package com.example.nasapictures;
+package com.example.nasapictures.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.nasapictures.R;
+
+public class PhotosGridActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_photos_grid);
     }
 }
