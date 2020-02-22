621a8b22811a74bd4202355290438d7674f1ea80
Vatsal Yadav <vatsalyad15@gmail.com>
2020-02-22 13:48:56 +0530

    Include PicturesGrid ModelView to grid activity and layout update
    PicturesGridActivity toolbar updated with custom toolbar using  displayToolbar() method.
    Initializing PicturesGridActivity using initPicturesGridModelView() method to get Picture Details and observe them for any changes.
    PicturesGridActivity uses initRecyclerView() method to initialize pictures grid recycler view to show NASA picture image and image name by PicturesGridRecyclerAdapter with GridLayout of two items in a row.
     Android activity layout activity_pictures_grid.xml has custom toolbar and RecyclerView for pictures grid

--- app/src/main/java/com/example/nasapictures/view/PicturesGridActivity.java
+++ app/src/main/java/com/example/nasapictures/view/PicturesGridActivity.java
@@ -3,14 +3,57 @@ package com.example.nasapictures.view;
 import android.os.Bundle;
 
 import com.example.nasapictures.R;
+import com.example.nasapictures.adapters.PicturesGridRecyclerAdapter;
+import com.example.nasapictures.models.PictureDetails;
+import com.example.nasapictures.viewmodels.PicturesGridActivityViewModel;
 
+import java.util.List;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PicturesGridActivity extends AppCompatActivity {
+
+    private static final String TAG = "PhotosGridActivity";
+
+    private RecyclerView mRecyclerView;
+    private PicturesGridRecyclerAdapter mPicturesGridRecyclerAdapter;
+    private PicturesGridActivityViewModel mPicturesGridActivityViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pictures_grid);
+        displayToolbar();
+        initPicturesGridModelView();
+        initRecyclerView();
+    }
+
+    private void displayToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.home_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void initPicturesGridModelView() {
+        mPicturesGridActivityViewModel = new ViewModelProvider(this).get(PicturesGridActivityViewModel.class);
+        mPicturesGridActivityViewModel.init(this);
+        mPicturesGridActivityViewModel.getPictureDetails().observe(this, new Observer<List<PictureDetails>>() {
+            @Override
+            public void onChanged(@Nullable List<PictureDetails> nicePlaces) {
+                mPicturesGridRecyclerAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private void initRecyclerView() {
+        mRecyclerView = findViewById(R.id.pictures_grid_recycler_view);
+        mPicturesGridRecyclerAdapter = new PicturesGridRecyclerAdapter(this, mPicturesGridActivityViewModel.getPictureDetails().getValue());
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+        mRecyclerView.setAdapter(mPicturesGridRecyclerAdapter);
     }
 }
